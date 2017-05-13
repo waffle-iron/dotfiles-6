74f30972cb11f6e2304b53984b96a181b5c732fd
Sean Norwood <norwood.sean@gmail.com>
2017-05-12 23:18:49 -0500

    Fix SC2124 and SC2086

--- scripts/fullscreen_wrap.sh
+++ scripts/fullscreen_wrap.sh
@@ -12,10 +12,10 @@
 # +   simply run the command and exit.
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-COMMAND="$@"
+COMMAND="$*"
 FOCUSED=$(xprop -root _NET_ACTIVE_WINDOW | awk -F' ' '{print $NF}')
 
-if xprop -id ${FOCUSED} _NET_WM_STATE | grep -q _NET_WM_STATE_FULLSCREEN; then
+if xprop -id "${FOCUSED}" _NET_WM_STATE | grep -q _NET_WM_STATE_FULLSCREEN; then
     i3-msg "[id=${FOCUSED}] fullscreen disable"
     ${COMMAND}
     i3-msg "[id=${FOCUSED}] fullscreen enable"
